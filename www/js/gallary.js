603475cefb2ced33f889a4c646eaad1763a68cab
Leonid <leoural66@yandex.ru>
2020-08-02 21:31:38 +0500

    Галлерея работ
    Начал делать галлерею работ

---
+++ www/js/gallary.js
@@ -0,0 +1,36 @@
+'use strict'
+// галлерея работ
+
+class Gallary {
+    constructor() {
+        this.urlGallry = '../img/gallary/';
+        this.gallryHTMLBlock = '';
+        this.blockGallry = document.querySelector('.portfolio__photos-block');
+        this.widthGallary = this.blockGallry.clientWidth;
+        this.width = 0;
+    }
+
+    creatPreviewGallary() {
+        for (let i = 1; i < 10; i++) {
+            this.gallryHTMLBlock = this.gallryHTMLBlock + `
+                <div class="gallary-preview" data-gallary="gal_${i}">
+                    <img src="${this.urlGallry}gal_${i}/1.jpg" data-image="gal_${i}">
+                </div>`
+        }
+        this.blockGallry.innerHTML = this.gallryHTMLBlock;
+    }
+
+    getClickBlock() {
+        this.blockGallry.addEventListener('click', (event) => {
+            console.log(event.target.dataset.image); // ! определение нажатой галлереи 
+        })
+    }
+
+    init() {
+        this.creatPreviewGallary();
+        this.getClickBlock();
+    }
+}
+
+let gallary = new Gallary();
+gallary.init();
