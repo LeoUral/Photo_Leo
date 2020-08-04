5d44d2f810c3fa66cd45b642c77f45aa5ba47a69
Leonid <leoural66@yandex.ru>
2020-08-04 20:25:07 +0500

    Мелкие правки

--- www/js/gallary.js
+++ www/js/gallary.js
@@ -8,10 +8,11 @@ class Gallary {
         this.blockGallry = document.querySelector('.portfolio__photos-block');
         this.widthGallary = this.blockGallry.clientWidth;
         this.width = 0;
+        this.quantity = 9; // количество галлерей для просмотра
     }
 
     creatPreviewGallary() {
-        for (let i = 1; i < 10; i++) {
+        for (let i = 1; i < (this.quantity + 1); i++) {
             this.gallryHTMLBlock = this.gallryHTMLBlock + `
                 <div class="gallary-preview" data-gallary="gal_${i}">
                     <img src="${this.urlGallry}gal_${i}/1.jpg" data-image="gal_${i}">
