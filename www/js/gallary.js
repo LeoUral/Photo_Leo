45708eff8315ef9b99024e0820103103cefeb65f
Leonid <leoural66@yandex.ru>
2020-08-24 22:27:56 +0500

    Правки
    Поменяно расположение меню, и оно фиксировано. Для просмотра фото в галерее добавлены стрелки. Увеличена яркость у ссылок на соц сети.

--- www/js/gallary.js
+++ www/js/gallary.js
@@ -15,6 +15,8 @@ class Gallary {
         this.arrImage = [];
         this.indexImage = 0;
         this.heightViewBlock = '';
+        this.arrowLeft;
+        this.arrowRight;
     }
 
     creatPreviewGallary() {
@@ -55,26 +57,53 @@ class Gallary {
 
     viewSelectedImage(index) {
         this.blockGallry.innerHTML = this.arrImage[index];
-        this.indexImage++;
+        // this.indexImage++;
         console.log(this.indexImage);
         this.sendHeightInStyleView();
     }
 
     viewInScreen() {
         this.viewSelectedImage(this.indexImage);
-        this.blockGallry.addEventListener('click', () => {
-            this.creatHandler()
-        });
+        // this.blockGallry.addEventListener('click', () => {
+        //     this.creatHandler()
+        // });
+        this.pressedLeftArrow();
+        this.pressedRightArrow();
         this.createClouseGallary();
     }
 
-    creatHandler() {
-        this.viewSelectedImage(this.indexImage);
-        if (this.indexImage < this.quantityImage) {
-            return;
-        } else {
+    // creatHandler() {
+    //     this.viewSelectedImage(this.indexImage);
+    //     if (this.indexImage < this.quantityImage) {
+    //         return;
+    //     } else {
+    //         this.indexImage = 0;
+    //     }
+    // }
+
+    chekIndexImage() {
+        if (this.indexImage > this.quantityImage - 1) {
             this.indexImage = 0;
+        } else if (this.indexImage < 0) {
+            this.indexImage = this.quantityImage - 1;
         }
+    }
+
+    pressedLeftArrow() {
+        this.arrowLeft = document.getElementById('left');
+        this.arrowLeft.addEventListener('click', () => {
+            this.indexImage--;
+            this.chekIndexImage();
+            this.viewSelectedImage(this.indexImage);
+        })
+    }
+    pressedRightArrow() {
+        this.arrowRight = document.getElementById('right');
+        this.arrowRight.addEventListener('click', () => {
+            this.indexImage++;
+            this.chekIndexImage();
+            this.viewSelectedImage(this.indexImage);
+        })
     }
 
     sendHeightInStyleView() {
@@ -84,8 +113,11 @@ class Gallary {
     creatButtonClouse() {
         let clouseBtn = document.querySelector('.clouse-btn');
         clouseBtn.insertAdjacentHTML('afterend', `
-                    <button id="clousebtn" class="clousebtn">закрыть просмотр</button>
-                    <div class="text-click">нажмите фото</div>`);
+                <div class="arrows">
+                    <button id="left" class="arrow left">&#10148;</button>
+                    <button id="clousebtn" class="clousebtn">&#10006;</button>
+                    <button id="right" class="arrow right">&#10148;</button>
+                </div>`);
     }
 
     createClouseGallary() {
