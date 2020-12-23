8b30b1d23ab59a3d42423d95b2d22f56b31e9aee
Leonid <leoural66@yandex.ru>
2020-12-23 21:54:36 +0500

    мелкие правки

--- www/js/verification.js
+++ www/js/verification.js
@@ -4,22 +4,23 @@ class Verification {
     constructor() {
         this.nameInput = document.getElementById('name');
         this.buttonSubmit = document.getElementById('button_submit');
+        this.textArea = document.getElementById('text');
         this.buttonSubmit.disabled = true; // отключаем кнопку
     }
     getNameInput() {
         this.nameInput.oncut = this.nameInput.oncopy = this.nameInput.onpaste = function() {
             return false;
         };
+        this.textArea.oncut = this.textArea.omcopy = this.textArea.onpaste = function() {
+            return false;
+        };
         this.nameInput.addEventListener('change', () => {
-            console.log(this.nameInput.value);
             if (this.nameInput.value != null) {
                 this.onButtonSubmit()
                 this.buttonSubmit.disabled = false; // включаем кнопку
             }
         });
     }
-
-
 
     onButtonSubmit() {
         this.buttonSubmit.classList.remove('btn-off');
